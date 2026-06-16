4e942121e3adc75d27e129684a27c404c66048f0
byronfire2004 <jesuspereiratonagmail.com>
2026-06-16 00:34:50 -0400

    mensaje de errores

---
+++ assets/arreglos.js
@@ -0,0 +1,5 @@
+const errores = [
+  "El campo no puede estar vacío.",
+  "El correo no es válido.",
+  "La contraseña es muy corta."
+];
--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,6 @@
-const meses
+function mostrarError() {
+  const indice = Math.floor(Math.random() * errores.length);
+  const caja = document.getElementById("mensaje");
+  caja.textContent = "❌ " + errores[indice];
+  caja.style.display = "block";
+}
